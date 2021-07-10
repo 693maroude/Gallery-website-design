95f4c1b8acfa7ae1ebc6c7f7be6539eef9353bb9
BBP <bishantpant2010@gmail.com>
2021-07-11 00:16:23 +0545

    made few changes

--- src/index.js
+++ src/index.js
@@ -19,9 +19,10 @@ addpic = () => {
   ];
   let div = document.createElement("div");
   div.classList.add("gallery-item");
+  div.setAttribute("ontouchstart", `item_touched(${index})`);
   div.innerHTML = `
   <img class="gallery-item-img" src="https://source.unsplash.com/random/?${search[index]}"/>
-  <div class="gallery-item-after">
+  <div class="gallery-item-after-0 gallery-item-after">
     <div class="gallery-item-after-label">labels</div>
     <div class="gallery-item-after-body">
       <div class="gallery-item-after-title">Title</div>
@@ -37,12 +38,37 @@ for (i = 0; i < 15; i++) {
   setTimeout(addpic, 0);
 }
 
-function transformScroll(event) {
+item_touched = (itemIndex) => {
+  let gallery_item_after =
+    document.getElementsByClassName("gallery-item-after")[itemIndex];
+  if (gallery_item_after.className.indexOf("gallery-item-after-0") >= 0) {
+    console.log("changed opacity to 1 ", gallery_item_after.className);
+    gallery_item_after.classList.remove("gallery-item-after-0");
+    gallery_item_after.classList.add("gallery-item-after-1");
+  } else {
+    console.log("changed opacity to 0 ", gallery_item_after.className);
+    gallery_item_after.classList.remove("gallery-item-after-1");
+    gallery_item_after.classList.add("gallery-item-after-0");
+  }
+
+  // let gallery_item_after =
+  //   document.getElementsByClassName("gallery-item-after")[itemIndex];
+  // let opacity = window
+  //   .getComputedStyle(gallery_item_after)
+  //   .getPropertyValue("opacity");
+  // console.log(opacity);
+  // if (opacity !== "0") {
+  //   gallery_item_after.style.opacity = 0;
+  // } else {
+  //   gallery_item_after.style.opacity = 1;
+  // }
+};
+
+transformScroll = (event) => {
   if (!event.deltaY) {
     return;
   }
   if (document.documentElement.clientWidth < 800) {
-    console.log("too small to scroll");
     return;
   }
 
@@ -53,7 +79,7 @@ function transformScroll(event) {
     left: +event.deltaY * 2,
     behavior: "smooth",
   });
-}
+};
 
 let element = document.getElementsByClassName("gallery")[0];
 element.addEventListener("wheel", transformScroll);
